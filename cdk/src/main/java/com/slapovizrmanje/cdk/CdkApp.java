35f2b24c6e5af08a774b34bd12c8e4a885cd35e2
Aleksandar Buljevic <aleksandar.buljevic@levi9.com>
2024-02-04 18:53:31 +0100

    Initialize boiler plate code for spring boot lambda function

---
+++ cdk/src/main/java/com/slapovizrmanje/cdk/CdkApp.java
@@ -0,0 +1,25 @@
+package com.slapovizrmanje.cdk;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.StackProps;
+
+public class CdkApp {
+    public static void main(final String[] args) {
+        final App app = new App();
+
+//        final String stage = System.getenv("DEPLOY_STAGE");
+//
+//        if (stage == null || stage.equals("")) {
+//            throw new RuntimeException("Please ensure that the \"DEPLOY_STAGE\" environment variable is set to a valid value.");
+//        }
+
+        new CdkStack(app, StackProps.builder()
+                .env(Environment.builder()
+                        .region("eu-central-1")
+                        .build())
+                .build());
+
+        app.synth();
+    }
+}
